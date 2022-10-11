e3830684a385b7bd3a3e4b65bcc3b3e8a0419c8d
Jyoti yadav <jyoti.yadav2508@gmail.com>
2022-10-11 11:50:54 +0000

    Apply ternary operator in incrementScore function and renderChoice function

--- game.js
+++ game.js
@@ -1,15 +1,17 @@
 const MAX_ROUNDS = 5;
-// let userChoice;
-// let computerChoice;
 let clickCount=0;
-// let userScore=0;
-// let computerScore=0;
 
-/**The complete game logic where user input is parsed, then random computer input is generated and logic is executed to determine the winner */
+
+/**The complete game logic where user input is parsed, 
+ * then random computer input is generated and 
+ * logic is executed to determine the winner.
+ * @param event the click event.
+  */
 
 function onUserSelection(event) {
-    // let clickCount;
-    if(clickCount >= MAX_ROUNDS) return;
+    if(clickCount >= MAX_ROUNDS){ 
+    alert("Game Over! Click Restart to play again");
+    return;}
 
     clickCount = ++clickCount;
     let userChoice = event.currentTarget.id;
@@ -20,45 +22,19 @@ function onUserSelection(event) {
 
     let compareResult = checkResult(userChoice, computerChoice);
     document.getElementById("compare-result").innerHTML = compareResult;
-    console.log(computerChoice, compareResult, clickCount);
-        // console.log(userScore, computerScore);
-
-
-    let userWon = checkResult(userChoice, computerChoice);
-    incrementScore(userWon);
-    displayWinner(userWon);
+    console.log(userChoice, computerChoice, clickCount);
 
     if(clickCount === 5){
         let winnerName = displayWinner();
         document.getElementById("winner").innerHTML = winnerName;
-        alert("Game Over! Restart again")
     } 
-
- 
-
-
-    if(clickCount >= MAX_ROUNDS) {
-        gameOver();
-    }
 }
-
 function renderChoice(isUser, choice) {
     let choiceElement = isUser ? "user-choice": "computer-choice";
 
     document.getElementById(choiceElement).innerHTML = `<i class= "fa-solid fa-hand-${choice} fa-2x"></i>`
 }
-
-// let compareResult = checkResult();
-// document.getElementById("compare-result").innerHTML = compareResult;
-// console.log(computerChoice, compareResult, clickCount);
-    // console.log(userScore, computerScore);
-
-// if(clickCount === 5){
-//     let winnerName = displayWinner();
-//     document.getElementById("winner").innerHTML = winnerName;
-//     alert("Game Over! Restart again")
-// }    
-
+  
 function getComputerChoice(){
     let randomNumber = Math.random();
     if(randomNumber < 0.33){
@@ -70,29 +46,36 @@ function getComputerChoice(){
     }
 }
 
-function checkResult(userChoice, computerChoice) {
+function checkResult(userChoice, computerChoice) 
+{
     if(userChoice === computerChoice){
         return "It's a draw!";
-    }else if ((userChoice === "rock" && computerChoice === "paper") || 
+      }
+    else if ((userChoice === "rock" && computerChoice === "paper") || 
              (userChoice === "paper" && computerChoice === "scissors") || 
              (userChoice === "scissors" && computerChoice === "rock")) 
     {
-    incrementScore(false);
-    return `Computer Wins! (${computerChoice} beats ${userChoice})`;
-} else {
-  incrementScore(true);
-  return `User Wins!  (${userChoice} beats ${computerChoice})`;
-} 
+        incrementScore(false);
+        return `Computer Wins! (${computerChoice} beats ${userChoice})`;
+    }   
+    else 
+    {
+        incrementScore(true);
+        return `User Wins!  (${userChoice} beats ${computerChoice})`;
+    } 
 }
 
 function incrementScore(userWon){
     if(userWon === undefined) return;
-
+    
     let scoreElement = userWon ? "user-score" : "computer-score";
     score = parseInt(document.getElementById(scoreElement).innerHTML);
     document.getElementById(scoreElement).innerHTML = ++score;
 }
-function displayWinner(userScore, computerScore) {
+
+function displayWinner() {
+    let userScore = parseInt(document.getElementById("user-score").innerHTML);
+    let computerScore = parseInt(document.getElementById("computer-score").innerHTML);
     if (userScore > computerScore) {
         return "Congratulations ! User Wins...";
     } else if (userScore < computerScore) {
