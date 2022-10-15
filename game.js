e8deaa7ac2d12b7f52971b98b8266944b3a69826
Jyoti yadav <jyoti.yadav2508@gmail.com>
2022-10-15 19:09:09 +0000

    Write additional comments for functions in JS file

--- game.js
+++ game.js
@@ -1,23 +1,19 @@
-/**
- * Total number of selections per game.
- */
+//Total number of selections per game
 const MAX_ROUNDS = 5;
-/**
- * The current count of how many times user makes a move.
- */
+
+//The current count of how many times user  makes a selection 
 let clickCount = 0;
+
 /**
  * The complete game logic where user input is parsed, 
  * then random computer input is generated and 
  * logic is executed to determine the winner.
- * @param event the click event.
+ * @param event the click event
  */
-
 function onUserSelection(event) {
     
     if (clickCount >= MAX_ROUNDS) {
             return;
-
     }
 
     ++clickCount;
@@ -38,13 +34,18 @@ function onUserSelection(event) {
         
     }
 }
-
+/**
+ * When user makes a choice, render userChoice and computer choice
+ * @param {boolean} isUser-If true, its user choice. If false, computer choice
+ * @param {object} choice - user choice or computer choice
+ */
 function renderChoice(isUser, choice) {
     let choiceElement = isUser ? "user-choice" : "computer-choice";
 
     document.getElementById(choiceElement).innerHTML =
      `<i class= "fa-solid fa-hand-${choice} fa-2x"></i>`
 }
+//Generate computer's random choice
 
 function getComputerChoice() {
     let randomNumber = Math.random();
@@ -56,7 +57,12 @@ function getComputerChoice() {
         return "paper";
     }
 }
-
+/**
+ * Check the result of the selected click event and calls incrementScore function
+ * @param {object}  userChoice - User selection
+ * @param  {object} computerChoice - computer's random selection
+ * @returns {string} result of user and computer choice
+ */
 function checkResult(userChoice, computerChoice) {
     if (userChoice === computerChoice) {
         return "It's a draw!";
@@ -72,7 +78,11 @@ function checkResult(userChoice, computerChoice) {
         return `User Wins!  (${userChoice} beats ${computerChoice})`;
     }
 }
-
+/**
+ * Get the current score from the DOM and increment 
+ * by 1 either user score or computer score depending on checkResult
+ * @param userWon- true, user won. false, computer won 
+ */
 function incrementScore(userWon) {
     if (userWon === undefined) return;
 
@@ -80,7 +90,10 @@ function incrementScore(userWon) {
     let score = parseInt(document.getElementById(scoreElement).innerHTML);
     document.getElementById(scoreElement).innerHTML = ++score;
 }
-
+/**
+ * Displays the winText after comparing the user and 
+ * computer score
+ */
 function displayWinner() {
     let userScore = parseInt(document.getElementById("user-score").innerHTML);
     let computerScore = parseInt(document.getElementById("computer-score").innerHTML);
@@ -96,9 +109,10 @@ function displayWinner() {
     }
     document.getElementById("winner").innerHTML = winText;
 }
-
+// get the icon elements and add event listner to them
 function initGame() {
     let userOptions = document.querySelectorAll(".icon");
     userOptions.forEach((icon) => icon.addEventListener("click", onUserSelection));
 }
+//Wait for the DOM to finish loading before running the game
 document.addEventListener("DOMContentLoaded", initGame);
